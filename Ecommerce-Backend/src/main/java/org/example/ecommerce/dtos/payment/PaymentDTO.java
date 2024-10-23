4e3f3529e71e0a4ce311619e784d2442486ee8e1
sayed <sayedhassan800@gmail.com>
2024-10-23 23:01:52 +0300

    Add payment dtos

---
+++ Ecommerce-Backend/src/main/java/org/example/ecommerce/dtos/payment/PaymentDTO.java
@@ -0,0 +1,38 @@
+package org.example.ecommerce.dtos.payment;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.ecommerce.models.Customer;
+import org.example.ecommerce.models.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PaymentDTO {
+    private int amount;
+    private String currency;
+    private String notification_url;
+    private int expiration;
+    private List<Object> payment_methods = new ArrayList<>();
+    private BillingDataDTO billing_data;
+    private CustomerPaymentDTO customer;
+    private ExtrasDTO extras;
+
+    public PaymentDTO(Customer customer, Order order) {
+        this.customer = new CustomerPaymentDTO(customer);
+        this.extras = new ExtrasDTO(order.getId());
+        this.billing_data = new BillingDataDTO(customer, order);
+        this.amount = order.getTotalPrice();
+        this.currency = "EGP";
+        this.expiration = 60*15;
+        this.payment_methods.add(4719614);
+        Dotenv dotenv = Dotenv.load();
+        this.notification_url = dotenv.get("TRANSACTION_PROCESSED_CALLBACK");
+    }
+
+}

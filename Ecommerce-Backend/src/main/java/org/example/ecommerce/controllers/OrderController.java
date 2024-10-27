bcecf5a63199d057baad38f3441af18a1f983076
sayed <sayedhassan800@gmail.com>
2024-10-27 18:36:57 +0300

    Add order history and order details
    - Add services and view order details to return all orders for admin
    
    - Add endpoint and service to retrieve the customer's own orders

--- Ecommerce-Backend/src/main/java/org/example/ecommerce/controllers/OrderController.java
+++ Ecommerce-Backend/src/main/java/org/example/ecommerce/controllers/OrderController.java
@@ -2,7 +2,10 @@ package org.example.ecommerce.controllers;
 
 import org.example.ecommerce.dtos.OrderResponseDTO;
 import org.example.ecommerce.dtos.OrderRequestDTO;
+import org.example.ecommerce.dtos.OrderViewDTO;
+import org.example.ecommerce.dtos.OrderWithItemsDTO;
 import org.example.ecommerce.models.OrderState;
+import org.example.ecommerce.models.PaymentMethod;
 import org.example.ecommerce.services.OrderService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,27 +24,60 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @GetMapping
-    public ResponseEntity<Page<OrderResponseDTO>> findAll(
-            @RequestParam(required = false, defaultValue = "1") Integer page,
+    @GetMapping("/admin")
+    public ResponseEntity<Page<OrderViewDTO>> findAllForAdmin(
+            @RequestParam(required = false, defaultValue = "0") Integer page,
             @RequestParam(required = false, defaultValue = "5") Integer size,
             @RequestParam(required = false) LocalDateTime startDate,
             @RequestParam(required = false) LocalDateTime endDate,
             @RequestParam(required = false) Integer minPrice,
             @RequestParam(required = false) Integer maxPrice,
-            @RequestParam(required = false) OrderState state,
-            @RequestParam(required = false) Long customerId) {
+            @RequestParam(required = false) List<OrderState> states,
+            @RequestParam(required = false) Long customerId,
+            @RequestParam(required = false) List<PaymentMethod> paymentMethods) {
 
         Map<String, Object> map = new HashMap<>();
-        if(size != null)map.put("state", state);
+        if(size != null)map.put("states", states);
         if(startDate != null)map.put("startDate", startDate);
         if(endDate != null)map.put("endDate", endDate);
         if(minPrice != null)map.put("minPrice", minPrice);
         if(maxPrice != null)map.put("maxPrice", maxPrice);
         if(customerId != null)map.put("customerId", customerId);
+        if(paymentMethods != null)map.put("paymentMethods", paymentMethods);
 
         return ResponseEntity.ok(orderService.findAllBySpecs(page, size, map));
 
+    }
+
+    @GetMapping("/customer")
+    public ResponseEntity<Page<OrderViewDTO>> findAll(
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "10") Integer size,
+            @RequestParam(required = false) LocalDateTime startDate,
+            @RequestParam(required = false) LocalDateTime endDate,
+            @RequestParam(required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice,
+            @RequestParam(required = false) List<OrderState> states,
+            @RequestParam(required = false) List<PaymentMethod> paymentMethods) {
+
+        Map<String, Object> map = new HashMap<>();
+        if(size != null)map.put("states", states);
+        if(startDate != null)map.put("startDate", startDate);
+        if(endDate != null)map.put("endDate", endDate);
+        if(minPrice != null)map.put("minPrice", minPrice);
+        if(maxPrice != null)map.put("maxPrice", maxPrice);
+        if(paymentMethods != null)map.put("paymentMethods", paymentMethods);
+
+        return ResponseEntity.ok(orderService.findAllBySpecs(page, size, map));
+
+    }
+    @GetMapping("/admin/{id}")
+    public ResponseEntity<OrderWithItemsDTO> findOrderForAdmin(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.findOrderWithId(id));
+    }
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<OrderWithItemsDTO> findOrderForCustomer(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.findOrderWithIdForCustomer(id));
     }
 
     @PostMapping

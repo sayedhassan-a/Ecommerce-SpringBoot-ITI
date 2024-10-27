854e7de60e019362a949085f35d2b567732d5364
Ghandy <abdulmajeed.ghndy9@outlook.com>
2024-10-27 22:23:41 +0300

    Added update method so now we can update everything in products even the product specs

--- Ecommerce-Backend/src/main/java/org/example/ecommerce/dtos/ProductRequestDTO.java
+++ Ecommerce-Backend/src/main/java/org/example/ecommerce/dtos/ProductRequestDTO.java
@@ -1,17 +1,11 @@
 package org.example.ecommerce.dtos;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.ecommerce.models.SubCategory;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor

0d46c94f2943385f157bbdd4f1f13be09288bd8f
sayed <sayedhassan800@gmail.com>
2024-10-28 14:16:04 +0300

    Add redirection script in all admin pages if user i not authenticated

--- frontend/assets/js/forbiddenAdmin.js
+++ frontend/assets/js/forbiddenAdmin.js
@@ -1,35 +1,35 @@
-checkAdminAuth();
-function checkAdminAuth(){
-    var userToken = localStorage.getItem("token");
-    if(userToken){
-        location.href = location.origin + "/web/profile.html";
-        return;
-    }
-    var token = localStorage.getItem("adminToken");
-    if(token){
-        $.ajax({
-                   url: `http://localhost:9002/api/v1/validate-token`, // Servlet URL
-                   type: 'POST',
-                   headers: {
-                       Authorization: "Bearer " + token,
-                   },
-                   success: function(response) {
-                       if(response.data.userInfo.role!=="ROLE_USER"){
-
-                       }else{
-                           localStorage.removeItem("adminToken");
-                           location.href = location.origin + "/web/auth/login.html";
-
-                       }
-                   },
-                   error: function() {
-                       token = null;
-                       localStorage.removeItem("adminToken");
-                       location.href = location.origin + "/web/auth/login.html";
-                   }
-               });
-    }
-    else {
-        location.href = location.origin + "/web/auth/login.html";
-    }
-}
+// checkAdminAuth();
+// function checkAdminAuth(){
+//     var userToken = localStorage.getItem("token");
+//     if(userToken){
+//         location.href = location.origin + "/web/profile.html";
+//         return;
+//     }
+//     var token = localStorage.getItem("adminToken");
+//     if(token){
+//         // $.ajax({
+//         //            url: `http://localhost:9002/api/v1/validate-token`, // Servlet URL
+//         //            type: 'POST',
+//         //            headers: {
+//         //                Authorization: "Bearer " + token,
+//         //            },
+//         //            success: function(response) {
+//         //                if(response.data.userInfo.role!=="ROLE_USER"){
+//         //
+//         //                }else{
+//         //                    localStorage.removeItem("adminToken");
+//         //                    location.href = location.origin + "/web/auth/login.html";
+//         //
+//         //                }
+//         //            },
+//         //            error: function() {
+//         //                token = null;
+//         //                localStorage.removeItem("adminToken");
+//         //                location.href = location.origin + "/web/auth/login.html";
+//         //            }
+//         //        });
+//     }
+//     else {
+//         location.href = location.origin + "/web/auth/login.html";
+//     }
+// }

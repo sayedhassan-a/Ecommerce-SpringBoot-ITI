2249fa4feb7463872071cc6d9dc64d3b59c77df2
sayed <sayedhassan800@gmail.com>
2024-10-29 03:31:55 +0300

    Add endpoint and service to delete credit card by id
    - Allow user role access to delete cards endpoint

--- Ecommerce-Backend/src/main/java/org/example/ecommerce/services/CreditCardService.java
+++ Ecommerce-Backend/src/main/java/org/example/ecommerce/services/CreditCardService.java
@@ -1,10 +1,14 @@
 package org.example.ecommerce.services;
 
+import jakarta.transaction.Transactional;
 import org.example.ecommerce.dtos.payment.CreditCardDTO;
 import org.example.ecommerce.models.CreditCard;
 import org.example.ecommerce.models.Customer;
 import org.example.ecommerce.repositories.CreditCardRepository;
 import org.example.ecommerce.repositories.CustomerRepository;
+import org.example.ecommerce.system.exceptions.exceptionHandling.exception.NotFoundException;
+import org.example.ecommerce.system.exceptions.exceptionHandling.exception.UnAuthorizedAccessException;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
@@ -14,9 +18,11 @@ import java.util.List;
 @Service
 public class CreditCardService {
     private final CustomerService customerService;
+    private final CreditCardRepository creditCardRepository;
 
-    public CreditCardService(CustomerService customerService) {
+    public CreditCardService(CustomerService customerService, CreditCardRepository creditCardRepository) {
         this.customerService = customerService;
+        this.creditCardRepository = creditCardRepository;
     }
 
     public List<CreditCardDTO> findAll() {
@@ -34,5 +40,21 @@ public class CreditCardService {
             creditCardDTO.setMaskedPan(creditCard.getMaskedPan());
             return creditCardDTO;
         })).toList();
+    }
+
+    @Transactional
+    public void deleteById(Long id){
+        String email = (String) ((Jwt) SecurityContextHolder.getContext()
+                                .getAuthentication().getPrincipal()).getClaims().get(
+                                "sub");
+
+        CreditCard creditCard =
+                creditCardRepository.findById(id).orElseThrow(()->new NotFoundException("No credit card with this id"));
+        if(creditCard.getCustomer().getEmail().equals(email)){
+            creditCardRepository.deleteById(id);
+            return;
+        }
+        throw new UnAuthorizedAccessException("User is not authorized to " +
+                "delete this credit card");
     }
 }
